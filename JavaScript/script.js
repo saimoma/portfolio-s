21011bd8248d52f8f61cbd61d6497f1399ded6af
saimomaaktersakila <saimomaakter@gmail.com>
2024-03-21 13:45:18 +0600

    saimoma

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,39 @@
 
+function projects(){
+    fetch('./portfolio.json')
+    .then(res => res.json())
+    .then(data => displayUsers(data))
+}
+projects();
+
+function displayUsers(data){
+    const seeDetails = document.getElementById('projects')
+ for(const users of data){
+    const div =document.createElement('div');
+    div.innerHTML=`
+    <img src="${users.image}"/>
+    <h3>${users.title}</h3>
+    <p>${users.descreption}</p>
+   <button><a href="${users.github}"><i class="fa-solid fa-code"></i></a></button>
+   <button><a href="${users.netlify}"><i class="fa-solid fa-eye"></i></a></button>
+    `;
+    div.classList.add('projectss')
+    seeDetails.appendChild(div);
+ 
+ console.log(users);
+ 
+ }
+}
+
+
+
+
+
+
+
+
+// <--------->
+
 const form = document.querySelector('form');
 const YourName = document.querySelector('#fname');
 const email = document.querySelector('#email');
